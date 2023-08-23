150f4333649954b1b8949ae5d40ff4f36b8ae570
y-09 <nyanyanyaworld@gmail.com>
2023-08-23 21:23:02 +0800

    Instantiates the dll in __init__.py

--- psplpy/__init__.py
+++ psplpy/__init__.py
@@ -1,3 +1,12 @@
+import ctypes
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+import glob
+
+package_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(package_dir)
+
+pydll = ctypes.CDLL(os.path.join(package_dir, r'lib\PyDll.dll'))
+
+py_files = glob.glob(os.path.join(package_dir, "*.py"))
+__all__ = [*py_files, pydll]
